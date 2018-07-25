043a0b361b2c435e506a5f0f0ba1b851707f7ffe
Ethan Coon <etcoon@gmail.com>
2018-07-25 13:47:32 -0400

    and tests

---
+++ workflow/test/test_files.py
@@ -0,0 +1,44 @@
+"""Test url generation"""
+
+import os
+import pytest
+import workflow.files
+
+import workflow.conf
+
+@pytest.fixture
+def source():
+    return workflow.files.HucNames(digits=4,
+                                   name='NHD Plus High Resoluton Hydrography',
+                                   url='https://www.url.gov',
+                                   base_folder='hydrography',
+                                   folder_template='NHDPlus_H_{0}_GDB',
+                                   file_template='NHDFlowline.shp')
+
+def test_parse_huc(source):
+    dd = workflow.conf.rcParams['data dir']
+    
+    assert(source.folder_name('0601') == os.path.join(dd,'hydrography/NHDPlus_H_0601_GDB'))
+    assert(source.folder_name('601') == os.path.join(dd,'hydrography/NHDPlus_H_0601_GDB'))
+    assert(source.folder_name('060101') == os.path.join(dd,'hydrography/NHDPlus_H_0601_GDB'))
+    with pytest.raises(ValueError):
+        source.folder_name('06')
+
+def test_derived(source):
+    dd = workflow.conf.rcParams['data dir']
+    assert(source.url('0601') == "https://www.url.gov/NHDPlus_H_0601_GDB.zip")
+    assert(source.file_name('0601') == os.path.join(dd,'hydrography/NHDPlus_H_0601_GDB/NHDFlowline.shp'))
+    assert(source.download('0601') == os.path.join(dd,'hydrography/zips/NHDPlus_H_0601_GDB.zip'))
+
+
+def test_download():
+    fm = workflow.files.NHDFileManager()
+    src = fm.names
+    print(src.download('0601'))
+    print(src.url('0601'))
+    workflow.files._download(src.url('0601'),src.download('0601'))
+    workflow.files._unzip(src.download('0601'), src.folder_name('0601'))
+
+
+    
+    
