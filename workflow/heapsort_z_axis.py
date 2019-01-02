9d2a329a495856e405fc324cf4d490030faf63a4
benliebersohn <ben.liebersohn@gmail.com>
2019-01-02 10:04:24 -0500

    updated and added warnings
    updates include stored sorted array, storing an array of tuples (2 pair: (id, z)) and sorting by the second values

--- workflow/heapsort_z_axis.py
+++ workflow/heapsort_z_axis.py
@@ -1,56 +1,9 @@
-from dataclasses import dataclass, field
-from typing import Any
-import itertools 
-
-@dataclass(order=True)
-class PrioritizedItem:
-    priority: int
-    item: Any=field(compare=False)
-pq = []                         # list of entries arranged in a heap
-entry_finder = {}               # mapping of points to entries
-REMOVED = '<removed-point>'      # placeholder for a removed point
-counter = itertools.count()     # unique sequence count
-
-def add_point(point, priority=0):
-    'Add a new point or update the priority of an existing point'
-    if point in entry_finder:
-        remove_point(point)
-    count = next(counter)
-    entry = [priority, count, point]
-    entry_finder[point] = entry
-    heappush(pq, entry)
-
-def remove_point(point):
-    'Mark an existing point as REMOVED.  Raise KeyError if not found.'
-    entry = entry_finder.pop(point)
-    entry[-1] = REMOVED
-
-def pop_point():
-    'Remove and return the lowest priority point. Raise KeyError if empty.'
-    while pq:
-        priority, count, point = heappop(pq)
-        if point is not REMOVED:
-            del entry_finder[point]
-            return point
-    raise KeyError('pop from an empty priority queue')
-
-#open the file, read to array 'list'
-test = open('numbers.vtk', 'r')
-list = {}
-for line in test:
-	words = line.split(" ")
-	list[words[0]] = words[3]
-	print(list)
-
-
-#outletPoint_id: name of the point which is designated as an outlet.
-#section id is not necessarially in order.
 in_file = open('test.vtk', 'r')
-z_value = [] #z_value is used for priority queue
-z_finder = {} #maps a node to a z_value
+elevation_tuples = [] #elevations are used for priority. See: z_sorted below
 REMOVED = '<removed-node>' #placeholder for a removed node
-section = 0
-counter = 0
+section = 0 #improve?
+counter = 0 #we count from 0 always
+outlet = [0, -1000] #node which is user defined
 
 for line in in_file:
 	if 'CELLS' in line:
@@ -60,10 +13,19 @@ for line in in_file:
 	if 'POINTS' in line:
 		print('Entering POINTS section 1')
 		section = 1
+		continue
 	if section == 1:
 		words = line.split(" ")
-		z_value = counter, words[2]
-		print(z_value)
+		elevation_tuples.append([int(counter), int(words[2])]) #groups tuples by (id, z_axis) and converts to ints
+		print(elevation_tuples)
 		counter += 1
+		continue
 
+z_sorted=(sorted(elevation_tuples, key=lambda x: x[1])) #save a sorted array of tuples, sorting by the second value in a pair (a z-axis vaue). The location of z-axis is set by x[1] which would be referring to a set with addresses [0,1] 
 
+if (z_sorted[0] != outlet): #checks to see if there's a big ole pit, or if something's off
+	print('WARNING: a pit appears to be below the outlet. This is not conforming.')
+	print(outlet)
+	print(z_sorted[0])
+
+print('Continuing...')
