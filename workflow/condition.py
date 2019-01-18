cc997b2c62a4babd958a532b6ecc1a34cf39c47a
Ethan Coon <etcoon@gmail.com>
2019-01-18 11:41:50 -0500

    adds script, second version of conditioning in a single pass

--- workflow/condition.py
+++ workflow/condition.py
@@ -1,5 +1,7 @@
 import numpy as np
+import sortedcontainers
 import attr
+import time
 
 @attr.s
 class Point:
@@ -19,42 +21,49 @@ def condition(points, outletID):
     """
 
     # create a sorted list of elevations, from largest to smallest
-    elev = sorted(list(points.items()), key=lambda id_p:-id_p[1].coords[2])
-
-    visited = set([outletID,])
-    pits = set()
+    elev = sortedcontainers.SortedList(list(points.items()), key=lambda id_p:id_p[1].coords[2])
     waterway = set([outletID,])
 
     # loop over elevation list from small to large
     while len(elev) is not 0:
-        current, current_p = elev.pop()
-        if current in visited:
+        current, current_p = elev.pop(0)
+        if current in waterway:
             # still in the waterway
-            waterway.add(current)
-            visited.update(current_p.neighbors)
+            waterway.update(current_p.neighbors)
         else:
-            # not in the waterway, add to pits
-            pits.add(current)
-    
-    print("Pits = %r"%(pits))
-    print("Waterway = %r"%(waterway))
-
-    # post-conditions
-    print(not pits.intersection(waterway))
-    assert(len(pits.union(waterway)) == len(points))
-
-    
-    # loop over waterway and raise up pits as they touch the waterway
-    waterway = sorted([ (ID,points[ID]) for ID in waterway], key=lambda id_p:-id_p[1].coords[2])
-    while len(waterway) is not 0:
-        current, current_p = waterway.pop()
-        for n in current_p.neighbors:
-            if n in pits:
-                points[n].coords[2] = max(current_p.coords[2], points[n].coords[2])
-                pits.remove(n)
-                waterway.append( (n,points[n]) )
-
-    # post-conditions
-    assert(len(pits) == 0)
+            # not in the waterway, fill
+            ww_neighbors = [n for n in current_p.neighbors if n in waterway]
+            if len(ww_neighbors) != 0:
+                current_p.coords[2] = min(points[n].coords[2] for n in ww_neighbors)
+            else:
+                current_p.coords[2] = min(points[n].coords[2] for n in current_p.neighbors)
+            
+            # push back into elev list with new, higher elevation
+            elev.add( (current,current_p) )
     return
 
+
+# if __name__ == "__main__":
+#     def make_points_1D(elevs):
+#         points = {}
+#         for i,e in enumerate(elevs):
+#             coords = np.array([i,0,e])
+#             if i == 0:
+#                 neighbors = [1,]
+#             elif i == len(elevs)-1:
+#                 neighbors = [i-1,]
+#             else:
+#                 neighbors = [i-1,i+1]
+#             points[i] = Point(coords, neighbors)
+#         return points
+
+#     def run_test_1D(elev_in, elev_out):
+#         points = make_points_1D(elev_in)
+#         condition(points, 0)
+
+#         for i in range(len(elev_in)):
+#             assert(points[i].coords[2] == elev_out[i])
+
+#     run_test_1D([0,1,3,2,1,4], [0,1,3,3,3,4])
+
+    
