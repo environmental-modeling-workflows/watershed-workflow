0eafb656a74ba853b089aee8cc65ff1d153b17fb
benliebersohn <ben.liebersohn@gmail.com>
2019-02-14 14:38:47 -0500

    append changes, conditions to append
    Since a point may have more than 2 neighbors, the new points must be appended. 
    Also, I added these if conditions to prevent duplicates. However a union or merge operation might be better?
    I believe the max number of neighbors is likely 6?
    Lastly it appears there's Points between PointId0 and mesh.GetNumberOfPoints() which have no neighbors. Is this possible?
    ex:
    10580: [9674, 10578], 10581: [], 10582: [], 10583: [], 10584: [10592, 10593], 10585: [], 10586: [],

--- pointID_printer.py
+++ pointID_printer.py
@@ -10,30 +10,27 @@ reader = vtk.vtkUnstructuredGridReader()
 reader.SetFileName('coweeta_basin2.vtk')
 reader.Update()
 mesh = reader.GetOutput()
-ncells = mesh.GetNumberOfCells() #I want number of points, though
-net = {c : [[],[]] for c in range(0,10851)} #needs to be allowed to grow! fix this.
-for c in range(10851): #npts
-    cell = mesh.GetCell(c)
-    #print("cell",c)
+ncells = mesh.GetNumberOfCells()
+npoints = mesh.GetNumberOfPoints()
 
- #   net = {c : [] for c in range(0,10)}
-    cell = mesh.GetCell(c)
- #   print(net)
+net = {c : [] for c in range(0,npoints)}
+for c in range(npoints): #npts loop over npoints? or ncells?
+    cell = mesh.GetCell(c) #load data for THIS cell in loop
     PointId0 = cell.GetPointId(0) #get the corner IDs for a cell
     PointId1 = cell.GetPointId(1)
     PointId2 = cell.GetPointId(2)
 
-    #if len(net[PointId0]) :
-    net[PointId0][0] = (PointId1) #add 1 and 2 as neighbors to 0
-    net[PointId0][1] = (PointId2)
-    print("id0",net[PointId0])
-    #if PointId1 not in ListOfLists:
-    net[PointId1][0] = (PointId0) #add 0 and 2 as neighbors to 1
-    net[PointId1][1] = (PointId2)
-    print("id1",net[PointId1])
-    #if PointId2 not in ListOfLists:
-    net[PointId2][0] = (PointId0) #add 0 and 1 as neighbors to 2
-    net[PointId2][1] = (PointId1)
-    print("id2",net[PointId2])
-    
+    if PointId1 not in net[PointId0]: # case 0 'do not continue on edges which we have already encountered'
+        net[PointId0] += [PointId1] # for each corner ID, we make a list
+    if PointId2 not in net[PointId0]:
+        net[PointId0] += [PointId2] # in these lists we append the ID of the neighbors, one cell at a time
+    if PointId0 not in net[PointId1]:
+        net[PointId1] += [PointId0] #add 0 and 2 as neighbors to 1
+    if PointId2 not in net[PointId1]:
+        net[PointId1] += [PointId2]
+    if PointId0 not in net[PointId2]:
+        net[PointId2] += [PointId0] #add 0 and 1 as neighbors to 2
+    if PointId1 not in net[PointId2]:
+        net[PointId2] += [PointId1]
+        
 print(net)
