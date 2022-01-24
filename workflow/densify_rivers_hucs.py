dc7e207ded28bbfdbe51f79c64d78c997b5d753e
SaubhagyaGT <saubhagya.rathore@outlook.com>
2022-01-24 15:15:07 -0500

    huc refinement updated
    added adaptive-refinement capability for hucs

--- workflow/densify_rivers_hucs.py
+++ workflow/densify_rivers_hucs.py
@@ -56,32 +56,60 @@ def DensifyNodeSegments(node,node_,limit=100,treat_collinearity=False):
         seg_coords_densified=TreatSegmentCollinearity(seg_coords_densified)
     return shapely.geometry.LineString(seg_coords_densified)
 
+def DensifyHucs(hucs,huc_,river,limit_scales=None):
 
-def DensifyHucs(hucs,huc_,limit=200):
-
-    """This function increases the resolution of huc boundary by adding equally spaced interpolated points
-     Arguments:
-      hucs              | hucs to be densified (workflow.split_hucs.SplitHUCs)
-      node_             | original huc containing all the known points fromthe source (workflow.split_hucs.SplitHUCs)
-      limit             | limit of section length above which more points are added
-    """
     # first if there are multiple segments, we define outer-ring and remove close points
     huc_ring=hucs.exterior().exterior.simplify(tolerance=1)
     coords=list(huc_ring.coords) 
     coords_=list(huc_.exterior().exterior.coords)
-    coords_densified=coords.copy()
+
+    if type(limit_scales)is list:
+        # basic refine
+        coords_densified_basic=DensifyHucs_(coords,coords_,river,limit_scales=limit_scales[-1])
+        # adaptive refine
+        coords_densified=DensifyHucs_(coords_densified_basic,coords_,river,limit_scales=limit_scales)
+        
+    else:
+        coords_densified=DensifyHucs_(coords,coords_,river,limit_scales=limit_scales)
+
+    return workflow.split_hucs.SplitHUCs([shapely.geometry.Polygon(coords_densified)])
+
+
+def DensifyHucs_(coords,coords_,river,limit_scales=None):
+
+    """This function increases the resolution of huc boundary by adding equally spaced interpolated points
+     Arguments:
+      hucs              | hucs to be densified (workflow.split_hucs.SplitHUCs)
+      huc_              | original huc containing all the known points fromthe source (workflow.split_hucs.SplitHUCs)
+      limit             | limit of section length above which more points are added
+    """
+    adaptive=type(limit_scales) is list # setting up flag
+    coords_densified=coords.copy() 
     j=0
     for i in range(len(coords)-1):
+
+        # calculation of limit for a set of point
+        if adaptive:
+            limit=limit_from_river_distance([coords[i],coords[i+1]],limit_scales,river)
+        else:
+            limit=limit_scales
+            
         section_length=math.dist(coords[i],coords[i+1])
+
         if section_length>limit:
             number_new_points=int(section_length//limit)
             end_points=[coords[i],coords[i+1]]  # points betwen which more points will be added
-            new_points=Interpolate(end_points,coords_,number_new_points)
+
+            if adaptive:
+                new_points=Interpolate_simple(end_points,number_new_points)
+            else:
+                new_points=Interpolate(end_points,coords_,number_new_points)
+                
             coords_densified[j+1:j+1]= new_points
             j+=number_new_points        
         j+=1 
 
-    return workflow.split_hucs.SplitHUCs([shapely.geometry.Polygon(coords_densified)])
+    return coords_densified
 
 
 
@@ -91,7 +119,7 @@ def Interpolate(end_points,interp_data,n):
 
     inds=[workflow.utils.closest_point(point,interp_data) for point in end_points] # point-indices on original network slicing a section for interpolation 
     if inds[1]<inds[0]: # this is to deal with corner case of interpolation of the last segment
-        inds[1]=-2        
+        inds[1]=-2     
     section_interp_data=np.array(interp_data[inds[0]:inds[1]+1]) # coordinates on section
     a=np.array(end_points); (dx,dy)=abs(a[0,:]-a[1,:])
     if dx>dy: # interpolating on x axis
@@ -102,6 +130,12 @@ def Interpolate(end_points,interp_data,n):
         f = interpolate.interp1d(section_interp_data[:,1],section_interp_data[:,0]) # creating interpolator 
         ynew=np.linspace(end_points[0][1],end_points[1][1], n+2)[1:-1] # new ys equally space between existing points
         xnew=f(ynew) # interpolated xs
+    new_points=[(xnew[k],ynew[k]) for k in range(n)]
+    return new_points
+
+def Interpolate_simple(end_points,n):
+    xnew=np.linspace(end_points[0][0],end_points[1][0], n+2)[1:-1] # new xs equally space between existing points
+    ynew=np.linspace(end_points[0][1],end_points[1][1], n+2)[1:-1] # new ys equally space between existing points
     new_points=[(xnew[k],ynew[k]) for k in range(n)]
     return new_points
 
@@ -129,3 +163,32 @@ def CheckCollinearity(p0, p1, p2, tol=1e-6):
     x1, y1 = p1[0] - p0[0], p1[1] - p0[1]
     x2, y2 = p2[0] - p0[0], p2[1] - p0[1]
     return abs(x1 * y2 - x2 * y1) < tol
+
+
+def limit_from_river_distance(segment_ends,limit_scales,river):
+    """Returns a graded refinement function based upon a distance function from rivers, for use with DensifyHucs function.
+
+    Triangle area must be smaller than near_area when the triangle
+    centroid is within near_distance from the river network.
+    Area must be smaller than away_area when the triangle
+    centroid is at least away_distance from the river network.
+    Area must be smaller than a linear interpolant between
+    near_area and away_area when between
+    near_distance and away_distance from the river
+    network.
+    """
+    near_distance, near_length, away_distance, away_length=limit_scales
+    p0=shapely.geometry.Point(segment_ends[0])
+    p1=shapely.geometry.Point(segment_ends[1])
+    p_mid=shapely.geometry.Point([(segment_ends[0][0]+segment_ends[1][0])/2,(segment_ends[0][1]+segment_ends[1][1])/2])
+    river_multiline = workflow.river_tree.forest_to_list([river])
+    distance=min(p0.distance(river_multiline),p_mid.distance(river_multiline),p1.distance(river_multiline))
+
+    if distance > away_distance:
+        length = away_length
+    elif distance < near_distance:
+        length = near_length
+    else:
+        length = near_length + (distance - near_distance) / (away_distance - near_distance) * (away_length - near_length)
+
+    return length
