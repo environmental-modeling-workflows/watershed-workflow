5478e0710fa05106a9b352c7f3fbe707bb720844
Ethan Coon <coonet@ornl.gov>
2022-08-19 11:40:21 -0400

    minor cleanup of docs, scripts

--- docker/build_user_container.sh
+++ docker/build_user_container.sh
@@ -1,4 +1,4 @@
-docker build --progress=plain -f docker/User-Env.Dockerfile -t ecoon/watershed_workflow:latest . && \
-docker build --progress=plain -f docker/ATS-User-Env.Dockerfile -t ecoon/watershed_workflow-ats:latest .
+docker build --progress=plain -f docker/User-Env.Dockerfile -t ecoon/watershed_workflow:master . && \
+docker build --progress=plain -f docker/ATS-User-Env.Dockerfile -t ecoon/watershed_workflow-ats:master .
 
 
