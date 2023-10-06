a40c82fff69c278923eff7001727df498d5bbb39
saubhagya-gatech <saubhagya.rathore@outlook.com>
2023-10-06 11:18:44 -0400

    ssr/ww 1.4 patch (#64)
    * supporting multiple rivers with internal boundaries
    
    * moving angle treatment to a separate function
    
    * documentation and examples update
    
    - Added user tips and comments in Taylor upstream example
    - Added angle treatment function call in Taylor upstream and Coweeta example for mixed-element meshes
    - Added capability to provide drainage-area-based function for quad widths

--- watershed_workflow/condition.py
+++ watershed_workflow/condition.py
@@ -434,6 +434,33 @@ def fill_gaps(img_in, nodata=np.nan):
     return interp0(np.ravel(xx), np.ravel(yy)).reshape(xx.shape)
 
 
+def condition_river_meshes(m2,
+                         rivers,
+                         smooth=False,
+                         use_parent=False,
+                         lower=False,
+                         use_nhd_elev=False,
+                         treat_banks=False,
+                         depress_upstream_by=None,
+                         network_burn_in_depth=None,
+                         ignore_in_sweep=None):
+    """For multile rivers, condition, IN PLACE, the elevations of stream-corridor elements
+    to ensure connectivity throgh culverts, skips ponds, maintain
+    monotonicity, or otherwise enforce depths of constructed channels."""
+    
+    for river in rivers:
+        condition_river_mesh(m2,
+                         river,
+                         smooth=smooth,
+                         use_parent=use_parent,
+                         lower=lower,
+                         use_nhd_elev=use_nhd_elev,
+                         treat_banks=treat_banks,
+                         depress_upstream_by=depress_upstream_by,
+                         network_burn_in_depth=network_burn_in_depth,
+                         ignore_in_sweep=ignore_in_sweep)
+    
+
 def condition_river_mesh(m2,
                          river,
                          smooth=False,
