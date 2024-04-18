2bed802a43f19c109128e11b2c68e2ff67d3d96b
saubhagya-gatech <saubhagya.rathore@outlook.com>
2024-04-18 16:08:25 -0400

    typo fixed

--- watershed_workflow/__init__.py
+++ watershed_workflow/__init__.py
@@ -1406,7 +1406,7 @@ def tessalate_river_aligned(hucs,
                                                                   ax=ax)
 
     # triangulate the rest
-    tri_res = watershed_workflow.triangulate(hucs_without_outlets,
+    tri_res = watershed_workflow.triangulate(hucs_without_outlet,
                                              rivers,
                                              corrs,
                                              internal_boundaries,
