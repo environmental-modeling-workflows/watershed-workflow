8c11f5a24d772fb91ff0a0091ce36d5e67d45406
Ethan Coon <coonet@ornl.gov>
2024-12-19 09:32:35 -0700

    adds in 3DEP

---
+++ watershed_workflow/sources/manager_3dep.py
@@ -0,0 +1,24 @@
+"""Manager for downloading 3DEP data."""
+
+from typing import Tuple
+
+import shapely
+import xarray
+import py3dep
+
+import watershed_workflow.crs
+
+class Manager3DEP:
+    name = '3DEP'
+    
+    def __init__(self, resolution):
+        self._resolution = resolution
+
+    def getDataset(self,
+                   geometry : shapely.geometry.Polygon | \
+                              shapely.geometry.MultiPolygon | \
+                              Tuple[float,float,float,float],
+                   geometry_crs : watershed_workflow.crs.CRS,
+                   dataset : str = "DEM") -> xarray.DataArray:
+        return py3dep.get_map(dataset, geometry, self._resolution, geometry_crs)
+        
