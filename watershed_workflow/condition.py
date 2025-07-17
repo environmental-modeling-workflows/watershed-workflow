48c4d1452e70ff2e4e5168aa248e127acbe824cd
saubhagya-gatech <saubhagya.rathore@outlook.com>
2025-07-17 12:49:56 -0400

    cleanup

--- watershed_workflow/condition.py
+++ watershed_workflow/condition.py
@@ -440,7 +440,7 @@ def conditionRiverMesh(m2 : Mesh2D,
                        use_parent : bool = False,
                        lower : bool = False,
                        bank_integrity_elevation : float = 0.0,
-                       depress_headwaters_by : Optional[float] = 0.0,
+                       depress_headwaters_by : Optional[float] = None,
                        network_burn_in_depth : Optional[float | Dict[int,float] | Callable[[River,], float]] = None,
                        known_depressions : Optional[List[int]] = None) -> None:
     """Condition, IN PLACE, the elevations of stream-corridor elements
