4c6b87a2ebfa7a0c6ecff62fcd1458103ec1c149
saubhagya-gatech <saubhagya.rathore@outlook.com>
2025-10-06 13:39:31 -0400

    export GeoDataFrame from m2 object with each cell as a row (#134)
    * export geoDataFrame from m2 object with each cell as a row
    
    * minor edit for consistency in name
    
    * changes name to to_dataframe to match those in split_hucs, river_tree, and adds a few more properties including crs to the dataframe
    
    ---------
    
    Co-authored-by: Ethan Coon <coonet@ornl.gov>

--- watershed_workflow/mesh.py
+++ watershed_workflow/mesh.py
@@ -26,6 +26,7 @@ import shapely
 import warnings
 import functools
 import pandas
+import geopandas as gpd
 from matplotlib import collections as mpc
 
 import watershed_workflow.crs
@@ -438,6 +439,22 @@ class Mesh2D:
         # toss geometry cache
         if hasattr(self, '_centroids'):
             del self._centroids
+            
+
+    def to_dataframe(self, include_labeled_sets : bool = False) -> gpd.GeoDataFrame:
+        """Convert the mesh to a GeoDataFrame with each cell as a row."""
+        vert_sets = [[self.coords[i, 0:2] for i in conn] for conn in self.conn]
+        polygons = [shapely.geometry.Polygon(verts) for verts in vert_sets]
+
+        gdf = gpd.GeoDataFrame(self.cell_data, geometry=polygons, crs=self.crs)
+
+        if include_labeled_sets:
+            for ls in self.labeled_sets:
+                if ls.entity == 'CELL':
+                    df[f'{ls.name} : {ls.setid}'] = gdf.index.isin(ls.ent_ids)
+
+        return df
+
 
     def plot(self,
              facecolors=None,
