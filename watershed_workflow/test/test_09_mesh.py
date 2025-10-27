49a605da4608fac71f8dea145cd2559c00a3ae00
Ethan Coon <coonet@ornl.gov>
2025-10-27 15:19:12 -0600

    prepartitioning (#136)
    adds prepartitioning support
    
    - adds ability to run pymetis to prepartition meshes
    - adds centroid calculations to m3 mesh
    - confirms on Coweeta, 16 core example that GIDs are preserved from WW
      to ATS to vis files/ats_xdmf
    - cleanup steadystate xml file for Coweeta example so observations are
      correct
    - update calls to ats_input_spec to do the right thing for partitioning

--- watershed_workflow/test/test_09_mesh.py
+++ watershed_workflow/test/test_09_mesh.py
@@ -51,10 +51,49 @@ def test_extrude():
     assert (6 * 11 == m3.num_vertices)
 
 
+def test_reorder():
+    m2 = watershed_workflow.mesh.Mesh2D.from_Transect(np.array([0, 10, 20, 30, 40, 50]),
+                                                      np.array([0,  0,  0,  0,  0,  0]),
+                                                      width=10)
+    assert m2.num_cells == 5
+    m2.labeled_sets.append(watershed_workflow.mesh.LabeledSet('myset', 101, 'CELL', [3,]))
+    assert np.allclose(m2.centroids[m2.labeled_sets[0].ent_ids[0], 0:2], [35.,0.], 1.e-10)
+
+    # reorder
+    new_order = [0, 4, 3, 1, 2]
+    m2new = m2.reorder(new_order)
+
+    # check the new mesh
+    assert m2new.num_cells == 5
+    assert np.allclose(m2new.centroids[0,0:2], [5.,0.], 1.e-10)
+    assert np.allclose(m2new.centroids[1,0:2], [45.,0.], 1.e-10)
+    assert np.allclose(m2new.centroids[4,0:2], [25.,0.], 1.e-10)
+    assert np.allclose(m2new.centroids[m2new.labeled_sets[0].ent_ids[0], 0:2], [35.,0.], 1.e-10)
+
+
+    # partition
+    m2newpart = m2new.partition(2, True)
+
+    assert m2new.num_cells == 5
+    # -- the partitioned mesh wants to put [0,20) on rank 1, (20,50] on rank 0
+    assert np.allclose(m2newpart.centroids[0,0:2], [ 5.,0.], 1.e-10)
+    assert np.allclose(m2newpart.centroids[1,0:2], [15.,0.], 1.e-10)
+    assert np.allclose(m2newpart.centroids[2,0:2], [45.,0.], 1.e-10)
+    assert np.allclose(m2newpart.centroids[3,0:2], [35.,0.], 1.e-10)
+    assert np.allclose(m2newpart.centroids[4,0:2], [25.,0.], 1.e-10)
+    assert np.allclose(m2newpart.centroids[m2newpart.labeled_sets[0].ent_ids[0], 0:2], [35.,0.], 1.e-10)
+
+    # check that the partition is good
+    assert 'partition' in m2newpart.cell_data
+    for i in range(1, len(m2newpart.cell_data['partition'])):
+        assert m2newpart.cell_data.loc[i-1, 'partition'] <= m2newpart.cell_data.loc[i,'partition']
+
+    
 def test_write():
     m2 = watershed_workflow.mesh.Mesh2D.from_Transect(np.array([0, 10, 20]),
                                                       np.array([0, 0, 0]),
                                                       width=10)
+    m2.cell_data['partition'] = [0,1]
     m3 = watershed_workflow.mesh.Mesh3D.extruded_Mesh2D(m2, ['constant', ], [5.0, ], [10, ],
                                                         [1001, ])
 
